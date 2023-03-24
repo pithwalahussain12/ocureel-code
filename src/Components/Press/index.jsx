d17b64bf7b93f578a82cb11160dc123bda79af4a
darshanboyat <106928085+darshanboyat@users.noreply.github.com>
2023-03-24 15:24:48 +0530

    Carousel

--- src/Components/Press/index.jsx
+++ src/Components/Press/index.jsx
@@ -18,7 +18,7 @@ const Index = () => {
             <p className='press__line'>We’re confident that Ocureel will revolutionize the way people interact online, and we’re excited to bring this game to the world. Our mission is to create an online community that celebrates honesty, authenticity, and positivity. We invite everyone to join us in this mission and see for themselves just how rewarding it can be to be SEEN and ACCEPTED for who you really are.</p>
 
             <p className='press__line'>It’s time to take a stand and make a big change! Join us in bringing life and connection back to the way we interact online. Get ready to download Ocureel and be seen for who you really are! Ocureel will be available on April 1st, 2023 in the Apple App Store.</p>
-            <p className='press__line'>For more information, please visit <a href="www.ocureel.com">www.ocureel.com.</a> For press inquiries, please contact <a href="mailto:press@ocureel.com">press@ocureel.com.</a></p>
+            <p className='press__line'>For more information, please visit <a href="https://ocureel.com/" target="_blank">www.ocureel.com.</a> For press inquiries, please contact <a href="mailto:press@ocureel.com">press@ocureel.com.</a></p>
         </div>
     )
 }
